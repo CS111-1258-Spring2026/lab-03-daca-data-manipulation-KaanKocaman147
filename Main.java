ed7ab7889478840bcbad14ff1ed07c493298f69d
KaanKocaman147 <kaan88x@gmail.com>
2026-02-11 22:08:53 +0000

    MATH

--- Main.java
+++ Main.java
@@ -4,65 +4,62 @@
 public class Main 
 {
   public static void main(String[] args) 
-	{
+  {
 
-		/***** DECLARATION SECTION *****/
-		int todayMonth, todayDay, todayYear;
-		int birthMonth, birthDay, birthYear;
-		
-		int a, y, m;
-		int jdnToday, jdnBirth;
-		int differenceDays;
-		int ageYears;
+    /***** DECLARATION SECTION *****/
+    int todayMonth, todayDay, todayYear;
+    int birthMonth, birthDay, birthYear;
+    
+    int a, y, m;
+    int jdnToday, jdnBirth;
+    int differenceDays;
+    int ageYears;
 
-		/***** INITIALIZATION SECTION *****/
-		todayMonth = 2;
-		todayDay = 22;
-		todayYear = 2026;
+    /***** INITIALIZATION SECTION *****/
+    todayMonth = 2;
+    todayDay = 2;
+    todayYear = 2020;
 
-		// Example birthday (change if needed)
-		birthMonth = 8;
-		birthDay = 26;
-		birthYear = 1918;
+    // Example birthday (change if needed)
+    birthMonth = 8;
+    birthDay = 26;
+    birthYear = 1918;
 
-		/***** INPUT SECTION *****/
-		// N/A
+    /***** CALCULATION & PROCESSING SECTION *****/
 
-		/***** CALCULATION & PROCESSING SECTION *****/
+    // JDN for today
+    a = (14 - todayMonth) / 12;
+    y = todayYear + 4800 - a;
+    m = todayMonth + 12 * a - 3;
 
-		// JDN for today
-		a = (14 - todayMonth) / 12;
-		y = todayYear + 4800 - a;
-		m = todayMonth + 12 * a - 3;
+    jdnToday = todayDay + (153 * m + 2) / 5 + 365 * y + y / 4 - y / 100 + y / 400 - 32045;
 
-		jdnToday = todayDay + (153 * m + 2) / 5 + 365 * y + y / 4 - y / 100 + y / 400 - 32045;
+    // JDN for birthday
+    a = (14 - birthMonth) / 12;
+    y = birthYear + 4800 - a;
+    m = birthMonth + 12 * a - 3;
 
-		// JDN for birthday
-		a = (14 - birthMonth) / 12;
-		y = birthYear + 4800 - a;
-		m = birthMonth + 12 * a - 3;
+    jdnBirth = birthDay + (153 * m + 2) / 5 + 365 * y + y / 4 - y / 100 + y / 400 - 32045;
 
-		jdnBirth = birthDay + (153 * m + 2) / 5 + 365 * y + y / 4 - y / 100 + y / 400 - 32045;
+    // Difference
+    differenceDays = jdnToday - jdnBirth ;
 
-		// Difference
-		differenceDays = jdnToday - jdnBirth;
+    // Approximate age using 365.25 to account for leap years
+    ageYears = (int)(differenceDays / 365.25);
 
-		// Approximate age
-		ageYears = differenceDays / 365;
-
-		/***** OUTPUT SECTION *****/
-		System.out.println("Julian Day Number for today's date, "
-				+ todayMonth + "/" + todayDay + "/" + todayYear
-				+ ", is " + jdnToday);
-		System.out.println();
-		System.out.println("Julian Day Number for birthday, "
-				+ birthMonth + "/" + birthDay + "/" + birthYear
-				+ ", is " + jdnBirth);
-		System.out.println();
-		System.out.println("The difference in days is " 
-				+ differenceDays 
-				+ ", which makes you approximately " 
-				+ ageYears 
-				+ " years old!");
+    /***** OUTPUT SECTION *****/
+    System.out.println("Julian Day Number for today's date, "
+        + todayMonth + "/" + todayDay + "/" + todayYear
+        + ", is " + jdnToday);
+    System.out.println();
+    System.out.println("Julian Day Number for birthday, "
+        + birthMonth + "/" + birthDay + "/" + birthYear
+        + ", is " + jdnBirth);
+    System.out.println();
+    System.out.println("The difference in days is " 
+        + differenceDays 
+        + ", which makes you approximately " 
+        + ageYears 
+        + " years old!");
   }
 }
